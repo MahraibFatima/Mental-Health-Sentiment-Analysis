a3b8ca4ef1cc393f5b4053ff5be8c5b23bef6bbf
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-09-21 20:18:11 +0000

    update common.py

--- src/MentalHealthAnalysis/utils/common.py
+++ src/MentalHealthAnalysis/utils/common.py
@@ -9,7 +9,6 @@ from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
-
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
@@ -34,8 +33,6 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     except Exception as e:
         raise e
     
-
-
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
@@ -62,9 +59,6 @@ def save_json(path: Path, data: dict):
         json.dump(data, f, indent=4)
 
     logger.info(f"json file saved at: {path}")
-
-
-
 
 @ensure_annotations
 def load_json(path: Path) -> ConfigBox:
@@ -109,8 +103,6 @@ def load_bin(path: Path) -> Any:
     logger.info(f"binary file loaded from: {path}")
     return data
 
-
-
 @ensure_annotations
 def get_size(path: Path) -> str:
     """get size in KB
@@ -123,5 +115,3 @@ def get_size(path: Path) -> str:
     """
     size_in_kb = round(os.path.getsize(path)/1024)
     return f"~ {size_in_kb} KB"
-
-
