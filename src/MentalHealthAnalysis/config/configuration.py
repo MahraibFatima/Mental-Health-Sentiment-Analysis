c59193f6bb6b3f668d1e5729315d3137e5bcb455
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-09-22 14:57:54 +0000

    update config

--- src/MentalHealthAnalysis/config/configuration.py
+++ src/MentalHealthAnalysis/config/configuration.py
@@ -20,20 +20,20 @@ class ConfigurationManager:
             self.schema = read_yaml(schema_filepath)
             
             # Create necessary directories
-            create_directories([self.config.get('artifacts_root', '')])
+            create_directories([self.config.getartifacts_root])
         except Exception as e:
             raise RuntimeError(f"Error loading configuration files: {e}")
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         try:
-            config = self.config.get('data_ingestion', {})
-            create_directories([config.get('root_dir', '')])
+            config = self.config.getdata_ingestion 
+            create_directories([config.root_dir])
 
             data_ingestion_config = DataIngestionConfig(
-                root_dir=config.get('root_dir', ''),
-                source_URL=config.get('source_URL', ''),
-                local_data_file=config.get('local_data_file', ''),
-                unzip_dir=config.get('unzip_dir', '') 
+                root_dir=config.root_dir,
+                source_URL=config.source_URL,
+                local_data_file=config.local_data_file,
+                unzip_dir=config.unzip_dir
             )
             return data_ingestion_config
         except Exception as e:
@@ -41,14 +41,14 @@ class ConfigurationManager:
     
     def get_data_validation_config(self) -> DataValidationConfig:
         try:
-            config = self.config.get('data_validation', {})
-            schema = self.schema.get('COLUMNS', {})
-            create_directories([config.get('root_dir', '')])
+            config = self.config.data_validation
+            schema = self.schema.COLUMNS
+            create_directories([config.root_dir])
 
             data_validation_config = DataValidationConfig(
-                root_dir=config.get('root_dir', ''),
-                STATUS_FILE=config.get('STATUS_FILE', ''),
-                unzip_data_dir=config.get('unzip_data_dir', ''),
+                root_dir=config.root_dir,
+                STATUS_FILE=config.STATUS_FILE,
+                unzip_data_dir=config.unzip_data_dir,
                 all_schema=schema,
             )
             return data_validation_config
@@ -57,12 +57,12 @@ class ConfigurationManager:
     
     def get_data_transformation_config(self) -> DataTransformationConfig:
         try:
-            config = self.config.get('data_transformation', {})
-            create_directories([config.get('root_dir', '')])
+            config = self.config.data_transformation
+            create_directories([config.root_dir])
 
             data_transformation_config = DataTransformationConfig(
-                root_dir=config.get('root_dir', ''),
-                data_path=config.get('data_path', ''),
+                root_dir=config.root_dir,
+                data_path=config.data_path,
             )
             return data_transformation_config
         except Exception as e:
@@ -71,19 +71,20 @@ class ConfigurationManager:
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         try:
             config = self.config.get('model_trainer', {})
-            params = self.params.get('xgboost_params', {})
+            params = self.params.get('XGBClassifier', {})
             schema = self.schema.get('TARGET_COLUMN', {})
 
-            create_directories([config.get('root_dir', '')])
+            create_directories([config.root_dir])
 
             model_trainer_config = ModelTrainerConfig(
-                root_dir=config.get('root_dir', ''),
-                train_data_path=config.get('train_data_path', ''),
-                test_data_path=config.get('test_data_path', ''),
-                model_name=config.get('model_name', ''),
-                alpha=params.get('alpha', 0.1),  # Default value if not present
-                l1_ratio=params.get('l1_ratio', 0.5),  # Default value if not present
-                target_column=schema.get('name', '')
+                root_dir=config.root_dir,
+                train_data_path=config.train_data_path,
+                test_data_path=config.test_data_path,
+                model_name=config.model_name,
+                learning_rate=params.get('learning_rate', 0.5),
+                max_depth=params.get('max_depth', 7),
+                n_estimators=params.get('n_estimators', 500),
+                target_column=schema.name
             )
             return model_trainer_config
         except Exception as e:
@@ -91,20 +92,23 @@ class ConfigurationManager:
 
     def get_model_evaluation_config(self) -> ModelEvaluationConfig:
         try:
-            config = self.config.get('model_evaluation', {})
-            params = self.params.get('xgboost_params', {})
-            schema = self.schema.get('TARGET_COLUMN', {})
+            config = self.config.model_evaluation
+            params = self.params.XGBClassifier
+            schema =  self.schema.TARGET_COLUMN
 
-            create_directories([config.get('root_dir', '')])
+            create_directories([config.root_dir])
 
             model_evaluation_config = ModelEvaluationConfig(
-                root_dir=config.get('root_dir', ''),
-                test_data_path=config.get('test_data_path', ''),
-                model_path=config.get('model_name', ''),  # Update to use model name
+                root_dir=config.root_dir,
+                test_data_path=config.test_data_path,
+                model_path = config.model_path,
                 all_params=params,
-                metric_file_name=config.get('metric_file_name', ''),
-                target_column=schema.get('name', '')
+                metric_file_name = config.metric_file_name,
+                target_column = schema.name
+           
             )
+
             return model_evaluation_config
+
         except Exception as e:
             raise RuntimeError(f"Error in model evaluation config: {e}")
